bd3709f9b688ca52f15c4af0bf20dc659fd919bd
RanieriC <51229092+RanieriC@users.noreply.github.com>
2019-09-17 12:23:34 +0200

    Create Main-File_A.js
    The Main Script that will run

---
+++ Main-File_A.js
@@ -0,0 +1,11 @@
+import AttackHeli.js
+import Choopper.js
+import GenderChange.js
+const("Attack Chopper") = method.attackchopper
+be that change(Gender) to == AttackHelicopter
+if response =% is shrek 2 isda bible
+respond =% yea boyyyyyyy
+if const(Gender) = Does not exist
+then femminist.act(CreateGender)
+then Print NSLog(Say) == ("I am an independent woman")
+then print NSlog(say) == ("i identify as the attack helicopter of shrek")
